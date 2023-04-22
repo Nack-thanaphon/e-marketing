1f9e43943d65c715c83355b63b6e91ed70d94671
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2023-04-22 12:26:28 +0700

    deploy API

--- src/app/service/ads-payment/ads-payment.component.ts
+++ src/app/service/ads-payment/ads-payment.component.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../app/environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-  
 export class AdsPaymentApi {
-
-  private apiUrl = 'http://localhost:8765/api';
+  private apiUrl = environment.apiUrl;
   private token = 'testToken';
 
   constructor(private http: HttpClient) {}
 
   getSomeData() {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.token}`
+    );
     return this.http.get(`${this.apiUrl}/paymentTotal`, { headers });
   }
-
-
 }
